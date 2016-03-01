808152a828b91b6f5e63d5e77d35e44f8689159b
Francisco Gómez <espectalll123@kydara.com>
2016-03-01 15:22:14 +0100

    Done stuff. I don't even remember.

--- julia/server.jl
+++ julia/server.jl
@@ -1,13 +1,5 @@
 #!/usr/bin/env julia
 
-#=
-Right now, I have no idea at all if Meryl supports
-any other parsing than the default XML one for POST
-It doesn't seem to care about the data's header...
-
-So yep. Right now, only send XML-written stuff.
-=#
-
 using Merly
 using JSON
 
